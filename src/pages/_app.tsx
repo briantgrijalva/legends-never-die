bdf5605ab41dd903f728df4b19d28432559ef697
Briant Grijalva <briantgrijalva20@gmail.com>
2023-06-13 18:56:04 -0600

    Initialize project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import RootLayout from './layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RootLayout>
+      <Component {...pageProps}/>
+    </RootLayout>
+  )
+
 }
---
+++ src/pages/layout.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+// import logo from '../assets/img/logo.svg'
+// import { font } from '@/assets/fonts/font'
+// import { FormattedMessage } from 'react-intl'
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+  return (
+    <>
+        <nav>
+          nav
+        </nav>
+        {children}
+        <footer>
+          footer
+        </footer>
+    </>
+  )
+}
