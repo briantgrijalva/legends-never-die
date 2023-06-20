4e493e87e0a5eb90d0a628977e5b04ef7de56f35
Briant Grijalva <briantgrijalva20@gmail.com>
2023-06-19 22:29:49 -0600

    Config tailwind

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,14 +3,15 @@ import Image from 'next/image'
 // import logo from '../assets/img/logo.svg'
 // import { font } from '@/assets/fonts/font'
 // import { FormattedMessage } from 'react-intl'
+import Navbar from '../components/Navbar';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-        <nav>
-          nav
-        </nav>
-        {children}
-        <footer>
+        <Navbar fixed={false} />
+        <main className='bg-gray-dark'>
+          {children}
+        </main>
+        <footer className='bg-gray'>
           footer
         </footer>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,36 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#1C2431',
+      'gray': '#2D3A50',
+      'gray-light': '#E0E5EE',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
